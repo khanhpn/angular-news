92cf9a25a5e6de52503682a79a6174b7be866a17
khanhpn <khanhpham@bravesoft.vn>
2017-10-12 15:42:52 +0900

    initialize project angular2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SubComponent } from './sub.component';
+import { StudyMultiComponent } from './multi-component/study-multi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubComponent,
+    StudyMultiComponent
   ],
   imports: [
     BrowserModule
